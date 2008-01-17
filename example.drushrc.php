648efadce9ed4e9652b60357f6bc786c00190c0a
weitzman <weitzman>
2008-01-17 05:41:36 +0000

    #198195 Allow drush to load an ini file during initialisation, to pre-configure options (such as path and site). patch by adrian. note that after this commit i am issueing a release candidate for this module.

---
+++ example.drushrc.php
@@ -0,0 +1,19 @@
+<?php
+// $Id: example.drushrc.php,v 1.1 2008/01/17 05:41:36 weitzman Exp $
+
+/*
+ * Examples of valid statements for a drushrc.php file. Use this file to cut down on
+ * typing of options and avoid mistakes.
+ *
+ * Rename this file to drushrc.php and
+ * optionally copy it to one of 5 convenient places. See drush_load_rc().
+ */
+
+// enable simulation mode
+// $options['s'] = 1;
+
+// specify a particular multisite
+// $options['l'] = 'http://example.com/subir';
+
+// enable verbose mode
+// $options['v'] = 1; 
