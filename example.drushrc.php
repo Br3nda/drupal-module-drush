a33bd678bd41a080d31b90e66199446c4baff733
weitzman <weitzman>
2008-01-23 14:57:39 +0000

    #212148 Provide a mechanism to override the global  array using drushrc.php. patch by adrian.

--- example.drushrc.php
+++ example.drushrc.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: example.drushrc.php,v 1.1 2008/01/17 05:41:36 weitzman Exp $
+// $Id: example.drushrc.php,v 1.2 2008/01/23 14:57:39 weitzman Exp $
 
 /*
  * Examples of valid statements for a drushrc.php file. Use this file to cut down on
@@ -17,3 +17,25 @@
 
 // enable verbose mode
 // $options['v'] = 1; 
+
+// use cvs checkouts when installing modules
+// an example of a command specific argument being set in drushrc.php
+// $options['handler'] = 'cvs';
+
+
+/**
+ * Variable overrides:
+ *
+ * To override specific entries in the 'variable' table for this site,
+ * set them here. Any configuration setting from the 'variable'
+ * table can be given a new value. We use the $override global here
+ * to make sure that changes from settings.php can not wipe out these
+ * settings.
+ *
+ * Remove the leading hash signs to enable.
+ */
+# $override = array(
+#   'site_name' => 'My Drupal site',
+#   'theme_default' => 'minnelli',
+#   'anonymous' => 'Visitor',
+# );
