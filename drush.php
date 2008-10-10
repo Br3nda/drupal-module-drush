d7374c3820564c3746176a8f3df22acfa16f6a68
weitzman <weitzman>
2008-10-10 15:30:05 +0000

    #310742 by scor, webchick. E_ALL notice fix - Undefined index: host

--- drush.php
+++ drush.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-// $Id: drush.php,v 1.27 2008/09/23 21:07:26 weitzman Exp $
+// $Id: drush.php,v 1.28 2008/10/10 15:30:05 weitzman Exp $
 
 /**
  * @file
@@ -97,9 +97,15 @@ function drush_bootstrap($argc, $argv) {
   }
 
   // Fake the necessary HTTP headers that Drupal needs:
-  $drupal_base_url = parse_url(DRUSH_URI);
-  $_SERVER['HTTP_HOST'] = $drupal_base_url['host'];
-  $_SERVER['PHP_SELF'] = $drupal_base_url['path'].'/index.php';
+  if (DRUSH_URI) {
+    $drupal_base_url = parse_url(DRUSH_URI);
+    $_SERVER['HTTP_HOST'] = $drupal_base_url['host'];
+    $_SERVER['PHP_SELF'] = $drupal_base_url['path'].'/index.php';
+  }
+  else {
+    $_SERVER['HTTP_HOST'] = NULL;
+    $_SERVER['PHP_SELF'] = NULL;
+  }
   $_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'] = $_SERVER['PHP_SELF'];
   $_SERVER['REMOTE_ADDR'] = '';
   $_SERVER['REQUEST_METHOD'] = NULL;
